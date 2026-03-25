6848a529154020820cc15f8b282bd5fc90ca70de
Jakub Rejmann <jrejmann@atlassian.com>
2026-03-25 21:28:29 +0100

    Initial Commit

---
+++ Majster/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Majster
+//
+//  Created by Jakub Rejmann on 25/03/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Majster/MajsterApp.swift
@@ -0,0 +1,17 @@
+//
+//  MajsterApp.swift
+//  Majster
+//
+//  Created by Jakub Rejmann on 25/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct MajsterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
